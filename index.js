2453852469f6561b336102513e2163bc947b64f2
pduren <57735971+kParkz@users.noreply.github.com>
2020-04-29 11:18:40 -0500

    fixed filler images in projects

--- index.js
+++ index.js
@@ -2,11 +2,12 @@
 const button1 = document.getElementById('button1');
 const button2 = document.getElementById('button2');
 const button3 = document.getElementById('button3');
+
+
 button1.addEventListener('click', function() {
     document.getElementById('top').classList.remove('weave');
     document.getElementById('top').classList.remove('graphing');
     document.getElementById('top').classList.add('honeycomb');})
-
 
 button2.addEventListener('click', function() {
     document.getElementById('top').classList.remove('honeycomb');
@@ -20,6 +21,4 @@ button3.addEventListener('click', function() {
     document.getElementById('top').classList.add('weave');
 })
 
-
-// document.getElementById('top').classList.remove('');
 
