7169de18d6e42d3feee77841639d8efcd8c6f303
pduren <57735971+kParkz@users.noreply.github.com>
2020-04-29 16:33:37 -0500

    added image gallery in projects

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+// handlers for changing background color on click (home page buttons)
 const button1 = document.getElementById('button1');
 const button2 = document.getElementById('button2');
 const button3 = document.getElementById('button3');
@@ -21,4 +21,20 @@ button3.addEventListener('click', function() {
     document.getElementById('top').classList.add('weave');
 })
 
+// image gallery for projects section
+function imageGallery() {
+    const highlight = document.querySelector('.gallery-highlight');
+    const previews = document.querySelectorAll('.project-preview img')
 
+    previews.forEach(preview => {
+        preview.addEventListener('click', function() {
+            const smallSrc = this.src;
+            const bigSrc = smallSrc.replace('small', 'big');
+            highlight.src = bigSrc;
+            previews.forEach(preview => preview.classList.remove('project-active'));
+            preview.classList.add('project-active');
+        })
+    })
+}
+
+imageGallery();
