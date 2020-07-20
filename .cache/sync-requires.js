30f519ba1915f2dca4a960002f3dc2bb0df13557
pduren <57735971+kParkz@users.noreply.github.com>
2020-07-20 13:16:24 -0500

    a commit

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -6,6 +6,7 @@ const preferDefault = m => m && m.default || m
 
 exports.components = {
   "component---cache-dev-404-page-js": hot(preferDefault(require("D:\\Repos\\park-playground\\portfolio-site\\.cache\\dev-404-page.js"))),
+  "component---src-pages-404-tsx": hot(preferDefault(require("D:\\Repos\\park-playground\\portfolio-site\\src\\pages\\404.tsx"))),
   "component---src-pages-index-tsx": hot(preferDefault(require("D:\\Repos\\park-playground\\portfolio-site\\src\\pages\\index.tsx")))
 }
 
