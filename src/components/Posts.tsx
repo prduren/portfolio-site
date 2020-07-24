314190f4f0e00628c5e538922bec47c5ec2cc918
pduren <57735971+kParkz@users.noreply.github.com>
2020-07-24 12:08:51 -0500

    gradient color change

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -23,26 +23,20 @@ const StyledPosts = styled.div`
       width: 20rem;
       margin-bottom: 2rem;
       transform: skew(-30deg);
-      background: linear-gradient(-45deg, #368f8b, #ddbea8,#246173, #ddbea8);
-	    background-size: 400% 400%;
-	    animation: gradient 5s ease infinite;
+      background: linear-gradient(-45deg, #368f8b, #ddbea8, #246173, #ddbea8);
+      background-size: 400% 400%;
+      animation: gradient 5s ease infinite;
       @keyframes gradient {
-    	0% {
-	    	background-position: 0% 50%;
-	    }
-    	50% {
-	    	background-position: 100% 50%;
-    	}
-    	100% {
-     	  background-position: 0% 50%;
-    	}
-}
-      /* background: linear-gradient(
-        to right,
-        ${(props) => props.theme.colors.brown},
-        ${(props) => props.theme.colors.green}
-      ); */
-
+        0% {
+          background-position: 0% 50%;
+        }
+        50% {
+          background-position: 100% 50%;
+        }
+        100% {
+          background-position: 0% 50%;
+        }
+      }
     }
   }
   @media screen and (max-width: 900px) {
